d61d9171c76a912bc3b0e3e990933cf990260380
HoRi0506 <vmflskgo10@naver.com>
2026-03-01 15:58:21 +0900

    Remove old test suite (18 files for deprecated modules)

--- tests/test_firecrawl.py
+++
@@ -1,9 +0,0 @@
-import os
-from tools.firecrawl_client import search
-
-
-def test_search_no_key_returns_empty():
-    # 키 미설정 시 빈 결과 (graceful degrade)
-    os.environ.pop("FIRECRAWL_API_KEY", None)
-    res = search("AI stocks", k=1)
-    assert isinstance(res, list)
--- tests/test_risk_edge.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from agents.risk_analyst import correlate
-
-
-def test_correlate_empty_input():
-    res = correlate([])
-    corr = res.get("corr")
-    assert isinstance(corr, pd.DataFrame)
-
